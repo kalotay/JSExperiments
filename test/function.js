e53932f76213294550099250c45e99262f3d9a84
Daniel Kalotay <d.kalotay@gmail.com>
2013-01-05 23:37:06 +0000

    dummy test

---
+++ test/function.js
@@ -0,0 +1,9 @@
+var _function = require('../lib/function')
+var asser = require('assert')
+
+describe('function', function () {
+  describe('variadic', function () {
+    it ('should work', function () {
+    })
+  })
+})
